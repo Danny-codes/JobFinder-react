97281318fd16c0bb785b313afe9e4ea4f5c425a5
Daniel <danielreisp.2020@gmail.com>
2022-03-11 14:46:02 -0300

    Starting the project, adding react-router-dom package

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <div>
+                <img id="logo" src="img/logo.png" alt="JobFinder" />
+                <ul>
+                    <li><>Encontrar vaga</></li>
+                    <li>< >Enviar vaga</></li>
+                    <li>< >Contato</></li>
+                </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
